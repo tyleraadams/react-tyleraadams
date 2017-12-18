a99576d8dd8589b2b3af0db2d0ad1a68f6c6ba68
Tyler Adams <tyleraadams@gmail.com>
2017-12-17 23:09:53 -0500

    disable button

--- src/components/tag_set/tag_set.js
+++ src/components/tag_set/tag_set.js
@@ -8,7 +8,18 @@ function TagSet(props) {
 
   return (
     <div className="tag-set">
+      <button
+        disabled={!doesAnySelectionExist}
+        type="button"
+        className={`tag-set__button ${doesAnySelectionExist
+          ? 'tag-set__button--active'
+          : 'tag-set__button--inactive'}`}
+        onClick={props.removeFilterAction}
+      >
+        Remove all filters
+      </button>
       <h4 className="tag-set__name">{props.name}</h4>
+
       <ul className="tag-set__list">
         {props.tags.map(function(tag, index) {
           const isSelected = props.selected.indexOf(tag) !== -1;
@@ -22,17 +33,6 @@ function TagSet(props) {
           );
         })}
       </ul>
-      {doesAnySelectionExist ? (
-        <button
-          type="button"
-          className="tag-set__button"
-          onClick={props.removeFilterAction}
-        >
-          Remove all filters
-        </button>
-      ) : (
-        ''
-      )}
     </div>
   );
 }
